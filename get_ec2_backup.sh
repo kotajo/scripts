c8b7eaa37d3c05491f5d56f7a6ff402988a7428a
kotajo <kotajo@gmail.com>
2016-08-30 08:23:07 +0900

    modify get_ec2_backup.sh

--- get_ec2_backup.sh
+++ get_ec2_backup.sh
@@ -10,7 +10,11 @@
 
 EC2_IDLIST="/tmp/ec2backup_idlist"
 YYYYMMDD=`date '+%Y%m%d'`
-LOGFILE=/tmp/get_ec2_backup_${YYYYMMDD}.log
+LOGFILE="/tmp/get_ec2_backup.log"
+
+function log() {
+  echo "`date "+%Y/%m/%d %H:%M:%S"` $1" >> ${LOGFILE}
+}
 
 
 ##########引数の数をチェック##########
@@ -26,11 +30,11 @@ ARG=$1
 
 #引数が --reboot または 指定なしの場合
 if [ -n ${ARG} ] || [ ${ARG} = "--reboot" ];then
-    CMD_OPT="--reboot "
+    CMD_OPT="--reboot"
 
 #引数が --no-rebootの場合
 elif [ ${ARG} = "--no-reboot" ];then
-    CMD_OPT="--no-reboot "
+    CMD_OPT="--no-reboot"
 
 else
     #それ以外の場合、エラーメッセージを標準出力とログに出力し、処理終了
@@ -40,15 +44,14 @@ fi
 
 
 ##########get-instance-ids##########
-#LOG
-echo "`date "+%Y/%m/%d %H:%M:%S"` START get-instance-ids" >> ${LOGFILE}
+log "START get-instance-ids"
 
 #AutoBackupタグがtrueになっているインスタンスのidのリストを取得
 aws ec2 describe-tags --filters "Name=tag:AutoBackup, Values=true" | grep "ResourceId" | awk -F'"' '{print $4}' > ${EC2_IDLIST}
 
 #LOG
 cat ${EC2_IDLIST} >> ${LOGFILE}
-echo "`date "+%Y/%m/%d %H:%M:%S"` END" >> ${LOGFILE}
+log "END"
 
 
 ##########エラーチェック用##########
@@ -56,49 +59,44 @@ echo "`date "+%Y/%m/%d %H:%M:%S"` END" >> ${LOGFILE}
 
 
 ##########create-image,tags##########
-#LOG
-echo "`date "+%Y/%m/%d %H:%M:%S"` START create-image from ${line}"
-
 #AMI取得、タグ付与をidリストに記載のインスタンスに繰り返す
 while read line
 do
     ##########create-image##########
-    #LOG
-    echo "`date "+%Y/%m/%d %H:%M:%S"` START create-image from ${line}" >> ${LOGFILE}
+    log "START create-image from ${line}"
+    ami_name="AMI_${line}_${YYYYMMDD}"
 
     #AMI取得、出力を変数へ格納
-    AMI_RET=`aws ec2 create-image --instance-id ${line} --name AMI_${line}_${YYYYMMDD} ${CMD_OPT} 2>&1`
+    ami_ret=`aws ec2 create-image --instance-id ${line} --name ${ami_name} ${CMD_OPT} 2>&1`
 
     #戻り値判定
     if [ $? = 0 ];then
        
        #正常（戻り値：0）の場合
-       #AMI取得完了までwait
-       aws ec2 wait image-available --filter "Name=name,Values=AMI_${line}_${YYYYMMDD}" 2>> ${LOGFILE}
-
        #出力されたAMI-idを変数へ格納
-       AMI_ID=`echo ${AMI_RET} | grep "ImageId" | awk -F'"' '{print $4}'`
+       ami_id=`echo ${ami_ret} | grep "ImageId" | awk -F'"' '{print $4}'`
        
-       #LOG
-       echo "`date "+%Y/%m/%d %H:%M:%S"` ${AMI_ID}" >> ${LOGFILE}
+       log ${ami_id}
 
     else
        #異常（戻り値：0以外）の場合、出力されたエラーをログに記入
-       echo "`date "+%Y/%m/%d %H:%M:%S"` ${AMI_RET}" >> ${LOGFILE}
+       log "ERROR : `echo ${ami_ret} | sed -e s/[\r\n]\+//g`"
     fi
 
-    #LOG
-    echo "`date "+%Y/%m/%d %H:%M:%S"` END" >> ${LOGFILE}
+    log "END"
 
 
     ##########create-tags##########
-    #LOG
-    echo "`date "+%Y/%m/%d %H:%M:%S"` START create-tags ${AMI_ID}" >> ${LOGFILE}
+    log "START create-tags ${ami_id}"
 
     #取得したAMIにタグを付与
-    aws ec2 create-tags --resources ${AMI_ID} --tags Key=BackupType,Value=Auto Key=CreateDate,Value=${YYYYMMDD} 2>> ${LOGFILE} 
+    tag_ret=`aws ec2 create-tags --resources ${ami_id} --tags Key=BackupType,Value=Auto Key=CreateDate,Value=${YYYYMMDD} 2>&1`
 
-    #LOG
-    echo "`date "+%Y/%m/%d %H:%M:%S"` END" >> ${LOGFILE}
+    #戻り値判定
+    if [ $? -ne 0 ];then
+        log "ERROR : `echo ${tag_ret} | sed -e s/[\r\n]\+//g`"
+    fi
+
+    log "END"
 
 done < ${EC2_IDLIST}
