f7f37910bd19a1c73d9ad44cecb0928b3178c75e
kotajo <kotajo@gmail.com>
2016-08-30 22:52:55 +0900

    ・Defaultを--no-rebootに変更 ・不要定数を削除 ・describe-tags失敗時の動作をエラーログ出力＆処理終了に変更 ・create-tagsの動作をcreate-image に成功した場合のみ実行に変更

--- get_ec2_backup.sh
+++ get_ec2_backup.sh
@@ -8,7 +8,6 @@
 #
 ###############################################
 
-EC2_IDLIST="/tmp/ec2backup_idlist"
 YYYYMMDD=`date '+%Y%m%d'`
 LOGFILE="/tmp/get_ec2_backup.log"
 
@@ -33,12 +32,12 @@ fi
 ##########引数の文字列をチェック##########
 ARG=$1
 
-#引数が --reboot または 指定なしの場合
-if [ -z ${ARG} ] || [ ${ARG} = "--reboot" ];then
-    CMD_OPT="--reboot"
+#引数が 指定なし または --no-reboot の場合 (Default)
+if [ -z ${ARG} ] || [ ${ARG} = "--no-reboot" ];then
+    CMD_OPT="--no-reboot"
 
-#引数が --no-rebootの場合
-elif [ ${ARG} = "--no-reboot" ];then
+#引数が --rebootの場合
+elif [ ${ARG} = "--reboot" ];then
     CMD_OPT="--no-reboot"
 
 else
@@ -51,14 +50,27 @@ fi
 ##########get-instance-ids##########
 log "START get-instance-ids"
 
-#AutoBackupタグがtrueになっているインスタンスのidのリストを取得
-incetance_id=`aws ec2 describe-tags --filters "Name=tag:AutoBackup, Values=true" | grep "ResourceId" | awk -F'"' '{print $4}'`
+#AutoBackupタグがtrueになっているインスタンス情報を取得
+INSTANCE_INFO=`aws ec2 describe-tags --filters "Name=tag:AutoBackup, Values=true" 2>&1`
+
+    #戻り値判定
+    if [ $? = 0 ];then
+
+       #正常（戻り値：0）の場合
+       #出力されたインスタンス情報からIDのみを変数へ格納
+       INSTANCE_IDS=`echo "${INSTANCE_INFO}" | grep "ResourceId" | awk -F'"' '{print $4}'`
+
+    else
+       #異常（戻り値：0以外）の場合、出力されたエラーメッセージを標準出力とログに出力し、処理終了
+       log_stdout "ERROR : `echo ${INSTANCE_INFO} | sed -e s/[\r\n]\+//g`"
+       exit 1
+    fi
 
 while read id
 do
     log ${id}
 done <<END
-${incetance_id}
+${INSTANCE_IDS}
 END
 
 log "END"
@@ -73,6 +85,8 @@ do
 
     #Nameタグの値を取得
     instance_name=`aws ec2 describe-tags --filters "Name=tag:Name, Values=*" "Name=resource-id, Values=${line}" | grep Value | awk -F'"' '{print $4}'`
+
+    #AMI名を定義
     ami_name="AMI_${instance_name}_${YYYYMMDD}"
     log ${ami_name}
 
@@ -84,31 +98,30 @@ do
        
        #正常（戻り値：0）の場合
        #出力されたAMI-idを変数へ格納
-       ami_id=`echo ${ami_ret} | grep "ImageId" | awk -F'"' '{print $4}'`
+       ami_id=`echo "${ami_ret}" | grep "ImageId" | awk -F'"' '{print $4}'`
        
        log ${ami_id}
+       log "END"
+
+
+       ##########create-tags##########
+       log "START create-tags ${ami_id}"
+
+       #取得したAMIにタグを付与
+       tag_ret=`aws ec2 create-tags --resources ${ami_id} --tags Key=BackupType,Value=Auto Key=CreateDate,Value=${YYYYMMDD} 2>&1`
+
+       #戻り値判定
+       if [ $? -ne 0 ];then
+           log "ERROR : `echo ${tag_ret} | sed -e s/[\r\n]\+//g`"
+       fi
 
     else
        #異常（戻り値：0以外）の場合、出力されたエラーをログに記入
        log "ERROR : `echo ${ami_ret} | sed -e s/[\r\n]\+//g`"
     fi
 
-    log "END"
-
-
-    ##########create-tags##########
-    log "START create-tags ${ami_id}"
-
-    #取得したAMIにタグを付与
-    tag_ret=`aws ec2 create-tags --resources ${ami_id} --tags Key=BackupType,Value=Auto Key=CreateDate,Value=${YYYYMMDD} 2>&1`
-
-    #戻り値判定
-    if [ $? -ne 0 ];then
-        log "ERROR : `echo ${tag_ret} | sed -e s/[\r\n]\+//g`"
-    fi
-
-    log "END"
+log "END"
 
 done <<END
-${incetance_id}
+${INSTANCE_IDS}
 END
